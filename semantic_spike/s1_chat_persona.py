acb395956b29b33fc2336871c38d93185191cb78
Tess Ferrandez <tferrand@microsoft.com>
2024-03-11 10:52:21 +0100

    update first spike

--- semantic_spike/s1_chat_persona.py
+++ semantic_spike/s1_chat_persona.py
@@ -1,3 +1,8 @@
-# add a system prompt to the chatbot
-# add history truncation (ex. drop oldest) / summarization
+# Can we have multiple chat services and switch between them?
 # retry logic - ex. if you don't like the response, we can try a different model
+# transparently switch between models as the conversation progresses - and the context window increases
+
+# Add a system prompt to the chatbot
+
+# Add history truncation (ex. drop oldest) / summarization
+# Conditionally summarizing the previous context - ex. if the previous context is too long, summarize it
